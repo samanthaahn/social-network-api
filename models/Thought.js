b49771d2beb1e66ae8c13274c6f7bf6507ea2fc6
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-05-10 17:08:23 -0700

    removed reaction from thought model and created it's own file

--- models/Thought.js
+++ models/Thought.js
@@ -11,7 +11,7 @@ const thoughtSchema = new mongoose.Schema(
     createdAt: {
       type: Date,
       defualt: Date.now,
-      get: (timestamp) => dateFormat(timestamp),
+      get: (date) => date.toLocaleDateString(),
     },
     username: {
       type: String,
@@ -27,35 +27,12 @@ const thoughtSchema = new mongoose.Schema(
   }
 );
 
-const reactionSchema = new mongoose.Scheme({
-  reactionId: {
-    type: mongoose.Schema.Types.ObjectId,
-    default: () => new mongoose.Types.OBjectId(),
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxLength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (timestamp) => dateFormat(timestamp),
-  },
-});
+
 
 thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
 })
 
-const Thought = mongoose.model('THought', thoughtSchema);
+const Thought = mongoose.model('Thought', thoughtSchema);
 
 module.exports = Thought; 
-
-function dateFormat(timestamp) {
-    return new Date(timestamp).toISOString();
-}
