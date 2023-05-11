7fab2d05b4108ce2bbb6f72baafaa5fb142f1962
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-05-10 17:43:19 -0700

    adding to thoughtController

---
+++ controllers/thoughtController.js
@@ -0,0 +1,52 @@
+const { Thought, User } = require('../models');
+
+updateThought,
+deleteThought,
+addReaction,
+deleteReaction
+
+modeule.exports = {
+    // Get all thoughts 
+getThoughts(req,res) {
+    Thought.find()
+    .then((thoughts) => res.status(200).json(thoughts))
+    .catch((error) => res.status(500).json(error))
+},
+// Gets a single thought by ID
+getSingleThought(req,res) {
+Thought.findById(req.params.thoughtId)
+.then((thoughts) => res.status(200).json(thoughts))
+.catch((error) => res.status(500).json(error))
+},
+// Create a thought 
+createThought(req, res) {
+    Thought.create(req.body)
+    .then((thought) => {
+        return User.findOneAndUpdate(
+            {username: req.body.username},
+            {$addToSet: {thoughts: thought._id}},
+            {new: true}
+        )
+    })
+    .then((thoughts) => res.status(200).json(thoughts))
+    .catch((error) => res.status(500).json(error(error)))
+},
+// Update a thought 
+updateThought(req,res) {
+    Thought.findOneAndUpdate(
+        {_id: req.params.thoughtId},
+        {$set: req.body},
+        {renValidators: true, new: true}
+    )
+    .then((thought) => 
+    !thought
+    ? res.status(400).json({ message: 'Sorry, no thought with that id!'})
+    : res.json(thought)
+    )
+},
+
+
+
+
+
+}
