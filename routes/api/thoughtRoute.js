b5542a3498ad91ae657b3f14b62d5486b48999a7
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-05-10 17:32:53 -0700

    added userRoute

--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -14,6 +14,6 @@ router.route('/').get(getThoughts).post(createThought);
 
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
-router.route('/: thoughtId/reaction').post(addReaction).delete(deleteReaction).delete(deleteReaction);
+router.route('/:thoughtId/reaction').post(addReaction).delete(deleteReaction);
 
 module.exports = router;
