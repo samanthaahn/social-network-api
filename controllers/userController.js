52d02e50f0de0754ffc024a4cfa75fcce2f6c74e
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-05-10 18:09:33 -0700

    addeded to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,43 +10,51 @@ module.exports = {
 // Get a single user
 getSingleUser(req, res) {
 User.findById(req.params.userId)
-.then
-.catch
-
-
+.then((users) => res.status(200).json(users))
+.catch((error) => res.status(500).json(error))
 },
 // Create a new user
 createUser(req, res) {
-
-    .then
-    .catch
-
+User.create(req.body)
+    .then((user) => res.status(200).json(user))
+    .catch((error) => res.status(500).json(error))
 },
 // Update a user
 updateUser(req, res) {
-    .then
-    .catch
-
-
+User.findOneAndUpdate(
+    {_id: req.params.userId},
+    {$set: req.body},
+    {runValidators: true, new: true}
+)
+    .then((user) => 
+    !user
+    ?res.status(404).json({message: 'Sorry, no user with that ID!'})
+    :res.json(user)
+    )
+    .catch((error) => res.status(500).json(error))
 },
 // Delete a user
 deleteUser(req, res) {
 
     .then
-    .catch
+    .catch((error) => res.status(500).json(error))
 
 },
 // Add a friend
 addFriend(req, res) {
+
+
     .then
-    .catch
+    .catch((error) => res.status(500).json(error))
 
 
 },
 // Remove a friend
 deleteFriend(req, res) {
+
+
     .then
-    .catch
+    .catch((error) => res.status(500).json(error))
 
 
 }
